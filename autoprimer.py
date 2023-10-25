f88ed7486b557f091bc402195678d0e935233363
Benjamin Fang <benjaminfang.ol@outlook.com>
2023-10-25 12:32:26 +0800

    complete info subcommand draft

--- autoprimer.py
+++ autoprimer.py
@@ -7,8 +7,8 @@ from biobrary.misc import change_coordinate
 from biobrary.seq import Seq
 
 def getargs_info(cmd_args):
-    parser = argparse.ArgumentParser(prog="autoprimer",
-                    description="A primer3 designer based on primer3.")
+    parser = argparse.ArgumentParser(prog="autoprimer info",
+                    description="print information of gene")
 
     parser.add_argument("--fasta", "-F", help="genome fasta file", required=True)
     parser.add_argument("--gtf", "-G", help="genome gtf file", required=True)
@@ -19,7 +19,7 @@ def getargs_info(cmd_args):
     parser.add_argument("--add_exonseq", "-C", action='store_true', help="add exon sequence informamtion")
     parser.add_argument("--anno_exonseq", action='store_true', help="add exon sequence informamtion")
     parser.add_argument("--add_cdsseq", "-D", action='store_true', help="add CDS sequence information")
-    
+
     parser.add_argument("--outfile", "-O", help="name of output file.",
                         default= None)
     parser.add_argument("geneid", nargs="+", help="ncbi sequence id")
@@ -43,6 +43,10 @@ def get_info(args):
         = getargs_info(args)
     fasta = FASTA(fasta)
     gtf = GTF(gtf)
+    if outfile:
+        outfile_fout = open(outfile, "w")
+    else:
+        outfile_fout = sys.stderr
     for geneid in geneids:
         gene = gtf.get_gene(geneid)
         geneid = gene.get_geneid()
@@ -50,7 +54,10 @@ def get_info(args):
         seqname = geneinfo[0]
         pos = geneinfo[2][0]
         ori = geneinfo[3]
-        print("\t".join([">" + geneid, seqname, ",".join([str(ele) for ele in pos]), ori]))
+        seq_len = str(pos[1] - pos[0] + 1)
+        print("\t".join([">" + geneid, seqname,
+                ",".join([str(ele) for ele in pos]), ori, seq_len]),
+                file=outfile_fout)
         seq = fasta.get_seq(seqname)
 
         if ori == "+":
@@ -80,9 +87,12 @@ def get_info(args):
             print("\t".join(["&UP", up_seq]))
             print("\t".join(["&GENESEQ", gene_seq]))
             print("\t".join(["&DOWN", down_seq]))
-                
-        gene_left = geneinfo[2][0][0]
-        gene_right = geneinfo[2][-1][-1]
+
+        if ori == "+":
+            ref_pos = geneinfo[2][0][0]
+        else:
+            ref_pos = geneinfo[2][-1][-1]
+
         trans = gene.get_transcript()
         for tran in trans:
             if tran.get_gbkey() == "mRNA":
@@ -90,7 +100,7 @@ def get_info(args):
                 traninfo = tran.get_info()
                 pos = traninfo[2]
                 ori = traninfo[3]
-                pos = change_coordinate(gene_left, pos, coor="relative", ori=ori)
+                pos = change_coordinate(ref_pos, pos, coor="relative", ori=ori)
                 print("\t".join(["@" + transid, ",".join([str(ele) for ele in pos[0]]), ori]))
                 if add_transseq:
                     trans_seq = gene_seq[pos[0] - 1: pos[1]]
@@ -102,7 +112,7 @@ def get_info(args):
                     start_info = start_codon.get_info()
                     pos = start_info[2]
                     ori = start_info[3]
-                    pos = change_coordinate(gene_left, pos, coor="relative", ori=ori)
+                    pos = change_coordinate(ref_pos, pos, coor="relative", ori=ori)
                     start_up = pos[0][0]
                     pos = [str(ele[0]) + "," + str(ele[1]) for ele in pos]
                     start_line = "\t".join(["$START", ";".join(pos), ori])
@@ -111,7 +121,7 @@ def get_info(args):
                     stop_info = stop_codon.get_info()
                     pos = stop_info[2]
                     ori = stop_info[3]
-                    pos = change_coordinate(gene_left, pos, coor="relative", ori=ori)
+                    pos = change_coordinate(ref_pos, pos, coor="relative", ori=ori)
                     stop_up = pos[0][0]
                     pos = [str(ele[0]) + "," + str(ele[1]) for ele in pos]
                     stop_line = "\t".join(["$STOP", ";".join(pos), ori])
@@ -120,74 +130,82 @@ def get_info(args):
                     cds_info = cds.get_info()
                     pos = cds_info[2]
                     ori = cds_info[3]
-                    pos = change_coordinate(gene_left, pos, coor="relative", ori=ori)
+                    pos = change_coordinate(ref_pos, pos, coor="relative", ori=ori)
                     pos_str = [str(ele[0]) + "," + str(ele[1]) for ele in pos]
                     cds_line = "\t".join(["$CDS", ";".join(pos_str), ori])
                     cds_seq = ""
                     if add_cdsseq:
                         for pp in pos:
                             cds_seq += gene_seq[pp[0] - 1: pp[1]]
-                 
+
                 if exon:
                     exoninfo = exon.get_info()
                     pos = exoninfo[2]
                     ori = exoninfo[3]
-                    pos = change_coordinate(gene_left, pos, coor="relative", ori=ori)
+                    pos = change_coordinate(ref_pos, pos, coor="relative", ori=ori)
                     pos_str = [str(ele[0]) + "," + str(ele[1]) for ele in pos]
                     exon_line = "\t".join(["$EXON", ";".join(pos_str), ori])
+
                     exon_seq = []
                     exon_seq_anno = []
-                    if add_exonseq:
-                        for pp in pos:
-                            seq_piece = gene_seq[pp[0] - 1: pp[1]]
-                            exon_seq.append(seq_piece)
-                            if start_up <= pp[1] and start_up >= pp[0]:
-                                seq_piece = list(seq_piece)
-                                seq_piece.insert(start_up - pp[0], "|>" + str(start_up) + "|")
-                                seq_piece = "".join(seq_piece)
-                            if stop_up <= pp[1] and stop_up >= pp[0]:
-                                seq_piece = list(seq_piece)
-                                seq_piece.insert(stop_up - pp[0], "|<" + str(stop_up) + "|")
-                                seq_piece = "".join(seq_piece)
-                            exon_seq_anno.append("".join(["[" + str(pp[0]) + "]", seq_piece, "[" + str(pp[1]) + "]"]))
-                        
-                        exon_seq = "".join(exon_seq)
-                        exon_seq_anno = "".join(exon_seq_anno)
+                    for pp in pos:
+                        seq_piece = gene_seq[pp[0] - 1: pp[1]]
+                        exon_seq.append(seq_piece)
+                        if start_up <= pp[1] and start_up >= pp[0]:
+                            seq_piece = list(seq_piece)
+                            seq_piece.insert(start_up - pp[0], "|>" + str(start_up) + "|")
+                            seq_piece = "".join(seq_piece)
+                        if stop_up <= pp[1] and stop_up >= pp[0]:
+                            seq_piece = list(seq_piece)
+                            seq_piece.insert(stop_up - pp[0], "|<" + str(stop_up) + "|")
+                            seq_piece = "".join(seq_piece)
+                        exon_seq_anno.append("".join(["[" + str(pp[0]) + "]", seq_piece, "[" + str(pp[1]) + "]"]))
 
-                         
+                    exon_seq = "".join(exon_seq)
+                    exon_seq_anno = "".join(exon_seq_anno)
+
+
                 print(exon_line)
-                print(exon_seq_anno)
-                print(exon_seq)
+                if anno_exonseq:
+                    print(exon_seq_anno)
+                elif add_exonseq:
+                    print(exon_seq)
 
                 print(cds_line)
-                print(cds_seq)
+                if add_cdsseq:
+                    print(cds_seq)
 
                 print(start_line)
                 print(stop_line)
-
-              
-
 
 
 def get_primer():
     pass
 
 
+def print_usage():
+    print("Usage:")
+    print("autoprimer subcommand -h/args\n")
+    print("subcommand:")
+    print("    info: print information of gene.")
+    print("    primer: design primer for gene.")
+    print()
+
+
 def main():
     args = sys.argv
-    if len(args) < 2:
-        print("Usage")
+    if len(args) < 2 or args[1] == "-h" or args[1] == "--help":
+        print_usage()
         exit()
 
     if args[1] == "info":
         get_info(args[2:])
-        
+
     elif args[1] == "primer":
         get_primer(args[2:])
     else:
-        print("Error, subcommand not recognized.", file=sys.stderr)
-
-
+        print_usage()
+        exit()
 
 
 if __name__ == "__main__":
